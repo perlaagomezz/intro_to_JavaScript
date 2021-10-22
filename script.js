9432a265987ba765ae36da79ffe4d7cf0b0eb957
perlaagomezz <gomezperla879@gmail.com>
2021-10-22 09:53:18 -0700

    made a style.css

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 console.log("Hello there I am here")
 
-var name = "Perla";
-var numb = 10;
-var numb = 1;
-console.log ( numb1 +numb2 );
+var name = "Perla"
+
+var numb = 10
+
+var numb2 = 1
+
+console.log(name);
+console.log(numb + numb2);
+
+function addition(numb2, numb){
+ console.log(numb + numb2);
+}
+
+function introduction( name,age){
+console.log('hello my name is ' + name + ' and I am' + age + ' years old' )
+}
+
+introduction('Perla', "16")
+
+
+
+console.log('My script is working');
+
+document.write("<h1>Intro to JavaScript</h1>");
+document.write("<h4>This is my project for CODE 201.</h4> ");
+
+alert('This is a alert on the my main page for my project.')
+
+alert('here is the second one on the main page .')
+
+alert('are you mad yet?');
+
+alert('OKAY IM DONEE!');
+
+alert('SIKKKEEE!');
+
+alert('okay im done have a nice day!')
+
+prompt('Tell me about your day?');
+prompt('why dont you like them?');
